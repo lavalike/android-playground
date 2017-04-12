f9f89e63ea366d61bc0d2ec276b93d8eaba5ee90
王震 <lavalike@yeah.net>
2017-04-12 11:04:15 +0800

    no message

--- app/src/main/java/com/android/exercise/ui/activity/RippleActivity.java
+++ app/src/main/java/com/android/exercise/ui/activity/RippleActivity.java
@@ -1,18 +1,33 @@
 package com.android.exercise.ui.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.Html;
 import android.util.Log;
+import android.view.Menu;
+import android.view.ViewConfiguration;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.android.exercise.R;
 import com.android.exercise.base.BaseActivity;
 import com.android.exercise.base.toolbar.ToolBarCommonHolder;
 import com.android.exercise.util.AppUtil;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
+import org.xml.sax.XMLReader;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 import butterknife.BindView;
@@ -26,16 +41,39 @@ public class RippleActivity extends BaseActivity {
 
     @BindView(R.id.tv_html)
     TextView tvHtml;
+    @BindView(R.id.tv_unique_id)
+    TextView tvUniqueId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ripple);
         ButterKnife.bind(this);
-        Log.e(TAG, getUniquePsuedoID());
+        setOverflowButtonAlways();
+        tvUniqueId.setText("UniqueID:" + getUniquePsuedoID());
         String html = AppUtil.getAssetsText("html/html_template.html");
-        tvHtml.setText(Html.fromHtml(html));
+        tvHtml.setText(Html.fromHtml(html, imgGetter, new Html.TagHandler() {
+            @Override
+            public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+
+            }
+        }));
     }
+
+    Html.ImageGetter imgGetter = new Html.ImageGetter() {
+        @Override
+        public Drawable getDrawable(String source) {
+            Log.e(TAG, Thread.currentThread().getName());
+            final Drawable[] drawable = {null};
+            Glide.with(mContext).load(source).asBitmap().into(new SimpleTarget<Bitmap>() {
+                @Override
+                public void onResourceReady(Bitmap bitmap, GlideAnimation<? super Bitmap> glideAnimation) {
+                    drawable[0] = new BitmapDrawable(bitmap);
+                }
+            });
+            return getResources().getDrawable(R.mipmap.ic_launcher);
+        }
+    };
 
     @Override
     protected void onSetupToolbar(Toolbar toolbar, ActionBar actionBar) {
@@ -62,5 +100,44 @@ public class RippleActivity extends BaseActivity {
         }
         //使用硬件信息拼凑出来的15位号码
         return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    private void setOverflowButtonAlways() {
+        ViewConfiguration config = ViewConfiguration.get(this);
+        try {
+            Field menuKey = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            menuKey.setAccessible(true);
+            menuKey.setBoolean(config, false);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
