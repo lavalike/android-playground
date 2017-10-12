efd01ec56697b01682c0695cff34b4f616924bdd
王震 <lavalike@yeah.net>
2017-10-12 13:19:36 +0800

    添加揭露动画

--- app/src/main/java/com/android/exercise/ui/activity/PorterDuffActivity.java
+++ app/src/main/java/com/android/exercise/ui/activity/PorterDuffActivity.java
@@ -1,22 +1,34 @@
 package com.android.exercise.ui.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import com.android.exercise.R;
 import com.android.exercise.base.BaseActivity;
 import com.android.exercise.base.toolbar.ToolBarCommonHolder;
+import com.android.exercise.ui.widget.RevealView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PorterDuffActivity extends BaseActivity {
+
+    @BindView(R.id.revealView)
+    RevealView revealView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_porter_duff);
         ButterKnife.bind(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                revealView.startRevealAnim();
+            }
+        }, 1000);
     }
 
     @Override
