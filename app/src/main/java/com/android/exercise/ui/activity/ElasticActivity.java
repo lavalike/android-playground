42ac38f3fc874d27423907209ff7d03a2a16cd8e
王震 <lavalike@yeah.net>
2018-01-20 11:15:55 +0800

    引入ElasticLayout

--- app/src/main/java/com/android/exercise/ui/activity/ElasticActivity.java
+++ app/src/main/java/com/android/exercise/ui/activity/ElasticActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import com.android.exercise.R;
 import com.android.exercise.base.BaseActivity;
 import com.android.exercise.base.toolbar.ToolBarCommonHolder;
-import com.android.exercise.ui.widget.PullLayout;
+import com.wangzhen.elastic.ElasticLayout;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -19,23 +19,23 @@ import java.net.URISyntaxException;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PullActivity extends BaseActivity {
+public class ElasticActivity extends BaseActivity {
 
     @BindView(R.id.webview)
     WebView webview;
-    @BindView(R.id.pull_layout)
-    PullLayout pullLayout;
+    @BindView(R.id.elastic_layout)
+    ElasticLayout elastic_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pull);
+        setContentView(R.layout.activity_elastic);
         ButterKnife.bind(this);
         String url = "https://mp.weixin.qq.com/s?__biz=MzIwMzYwMTk1NA==&amp;mid=2247483998&amp;idx=1&amp;sn=03cb1942533247ac23876448fdf1b39a&amp;chksm=96cda313a1ba2a050f8cc2325e36b468f620d3d167d9f0dbc3108127c5ba16d14dc83cabc3c6&amp;scene=21#wechat_redirect";
         webview.loadUrl(url);
-        View behindView = pullLayout.getBehindView();
+        View behindView = elastic_layout.getBehindView();
         if (behindView != null) {
-            ((TextView) behindView.findViewById(R.id.tv_tip)).setText(getUrlHost(url));
+            ((TextView) behindView.findViewById(R.id.tv_tip)).setText("网页由 " + getUrlHost(url) + " 提供");
         }
     }
 
