eea02a9ba7711716ce84599ec4a9e3fea1c02339
王震 <lavalike@yeah.net>
2018-03-16 17:19:07 +0800

    html

--- app/src/main/java/com/android/exercise/ui/activity/WebActivity.java
+++ app/src/main/java/com/android/exercise/ui/activity/WebActivity.java
@@ -3,14 +3,20 @@ package com.android.exercise.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.android.exercise.R;
 import com.android.exercise.base.BaseActivity;
-import com.android.exercise.base.toolbar.ToolBarCommonHolder;
+import com.android.exercise.base.toolbar.ToolBarRightTextHolder;
 import com.android.exercise.listener.IJSCallback;
 import com.android.exercise.ui.JSInterface;
+import com.android.exercise.ui.widget.dialog.ADWindowDialog;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,12 +40,33 @@ public class WebActivity extends BaseActivity implements IJSCallback {
         WebSettings settings = webview.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                ADWindowDialog adWindowDialog = new ADWindowDialog();
+                adWindowDialog.showDialog(getFragmentManager());
+                result.confirm();
+                return true;
+            }
+        });
         webview.addJavascriptInterface(new JSInterface(this), "android");
     }
 
     @Override
     protected void onSetupToolbar(Toolbar toolbar, ActionBar actionBar) {
-        new ToolBarCommonHolder(this, toolbar, getString(R.string.item_webview));
+        ToolBarRightTextHolder holder = new ToolBarRightTextHolder(this, toolbar, getString(R.string.item_webview), "点我");
+        holder.getRightMenu().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webview.evaluateJavascript("javascript:test()", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        Toast.makeText(mContext, value, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
@@ -50,5 +77,11 @@ public class WebActivity extends BaseActivity implements IJSCallback {
                 webview.loadUrl("javascript:getAppInfoAsyncCallback('" + data + "')");
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webview.destroy();
     }
 }
