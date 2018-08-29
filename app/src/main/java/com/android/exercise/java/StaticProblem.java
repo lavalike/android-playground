56840371d7cacc72ade20af4c7065a9d8a6827b9
王震 <lavalike@yeah.net>
2018-08-29 16:24:07 +0800

    static调用问题

---
+++ app/src/main/java/com/android/exercise/java/StaticProblem.java
@@ -0,0 +1,12 @@
+package com.android.exercise.java;
+
+public class StaticProblem {
+    public static void main(String[] args) {
+        StaticProblem problem = null;
+        problem.hello();
+    }
+
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
