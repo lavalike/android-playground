edf0dff554f44c7fce3f71ffe672ac1e74a9d624
王震 <lavalike@yeah.net>
2018-11-09 10:57:31 +0800

    原型模式、自定义Classloader、查找第一个不重复字符串

---
+++ app/src/main/java/com/android/exercise/java/classloader/Test.java
@@ -0,0 +1,7 @@
+package com.android.exercise.java.classloader;
+
+public class Test {
+    public void hello() {
+        System.out.println("我是由" + getClass().getClassLoader().getClass() + "加载进来的");
+    }
+}
