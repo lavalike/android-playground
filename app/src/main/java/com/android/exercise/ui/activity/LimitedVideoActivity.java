20e3157cc6f938eee2f1a197145b6014a90e1617
王震 <lavalike@yeah.net>
2019-02-11 12:05:56 +0800

    录像预览界面优化为16:9

--- app/src/main/java/com/android/exercise/ui/activity/LimitedVideoActivity.java
+++ app/src/main/java/com/android/exercise/ui/activity/LimitedVideoActivity.java
@@ -1,15 +1,19 @@
 package com.android.exercise.ui.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.exercise.R;
 import com.android.exercise.ui.widget.CameraSurfaceView;
-import com.android.exercise.util.UIUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,12 +32,38 @@ public class LimitedVideoActivity extends AppCompatActivity implements CameraSur
     @BindView(R.id.btn_opt)
     Button btnOpt;
 
+    //标准预览16:9
+    private static final float STANDARD_RATIO = 16f / 9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_limited_video);
         ButterKnife.bind(this);
+        optimizeRatio();
         surfaceView.setCallback(this);
+    }
+
+    /**
+     * 优化显示比例
+     */
+    private void optimizeRatio() {
+        WindowManager manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        if (manager == null) return;
+        Display display = manager.getDefaultDisplay();
+        if (display == null) return;
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int screenWidth = metrics.widthPixels;
+        int screenHeight = metrics.heightPixels;
+        float realRatio = screenHeight * 1f / screenWidth;
+        //屏幕比例大于16:9
+        if (realRatio > STANDARD_RATIO) {
+            //计算16:9实际对应的高度
+            int theoryHeight = (int) (screenWidth * STANDARD_RATIO);
+            ViewGroup.LayoutParams layoutParams = surfaceView.getLayoutParams();
+            layoutParams.height = theoryHeight;
+        }
     }
 
     @OnClick({R.id.btn_opt, R.id.btn_switch})
