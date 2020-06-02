6666a5f054b105217d24131d96c0e96cc4c0bfad
王震 <lavalike@yeah.net>
2020-06-02 15:14:33 +0800

    添加WorkManager页面

--- app/src/main/java/com/android/exercise/ui/MainActivity.java
+++ app/src/main/java/com/android/exercise/ui/MainActivity.java
@@ -70,6 +70,7 @@ import com.android.exercise.ui.activity.ViewAnimationActivity;
 import com.android.exercise.ui.activity.ViewSwitchActivity;
 import com.android.exercise.ui.activity.WaveActivity;
 import com.android.exercise.ui.activity.WindowActivity;
+import com.android.exercise.ui.activity.WorkManagerActivity;
 import com.android.exercise.ui.activity.calendar.CalendarActivity;
 import com.android.exercise.ui.activity.database.ObjectBoxActivity;
 import com.android.exercise.ui.activity.database.RealmActivity;
@@ -163,6 +164,8 @@ public class MainActivity extends BaseActivity {
 
     private List<BaseBean> generateList() {
         List<BaseBean> list = new ArrayList<>();
+        list.add(new TitleBean("任务调度"));
+        list.add(new ItemBean(getString(R.string.item_work_manager), WorkManagerActivity.class));
         list.add(new TitleBean("数据库"));
         list.add(new ItemBean(getString(R.string.item_object_box), ObjectBoxActivity.class));
         list.add(new ItemBean(getString(R.string.item_realm), RealmActivity.class));
