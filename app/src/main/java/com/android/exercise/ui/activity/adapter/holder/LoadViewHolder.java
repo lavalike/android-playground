68a17a5d932e0f9345c2ee999c028aee889588ab
王震 <lavalike@yeah.net>
2020-06-11 18:24:14 +0800

    dimeno-adapter:0.0.2

--- app/src/main/java/com/android/exercise/ui/activity/adapter/holder/LoadViewHolder.java
+++ app/src/main/java/com/android/exercise/ui/activity/adapter/holder/LoadViewHolder.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.android.exercise.R;
-import com.dimeno.adapter.RecyclerViewHolder;
+import com.dimeno.adapter.base.RecyclerViewHolder;
 
 /**
  * LoadViewHolder
