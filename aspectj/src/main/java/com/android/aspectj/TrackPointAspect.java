a1865996df8c351163c57d33dd9b0078b7e5f5f1
wangzhen <lavalike@yeah.net>
2020-08-19 10:12:06 +0800

    AspectJ点击、长按事件

--- aspectj/src/main/java/com/android/aspectj/TrackPointAspect.java
+++ aspectj/src/main/java/com/android/aspectj/TrackPointAspect.java
@@ -2,6 +2,7 @@ package com.android.aspectj;
 
 import android.content.res.Resources;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -15,12 +16,9 @@ import org.aspectj.lang.annotation.Pointcut;
  */
 @Aspect
 public class TrackPointAspect {
+
     @Pointcut("execution(* android.view.View.OnClickListener.onClick(..))")
     public void onViewClick() {
-    }
-
-    @Pointcut("execution(* android.view.View.OnLongClickListener.onLongClick(..))")
-    public void onViewLongClick() {
     }
 
     /**
@@ -51,31 +49,37 @@ public class TrackPointAspect {
         joinPoint.proceed();
     }
 
-//    /**
-//     * 拦截全局长按事件
-//     *
-//     * @param joinPoint joinPoint
-//     * @throws Throwable Throwable
-//     */
-//    @Around("onViewLongClick()")
-//    public void processLongClickJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
-//        String className = "";
-//        Object target = joinPoint.getTarget();
-//        if (target != null) {
-//            className = target.getClass().getName();
-//        }
-//        String entryName = "";
-//        Object[] args = joinPoint.getArgs();
-//        if (args.length > 0 && args[0] instanceof View) {
-//            View view = (View) args[0];
-//            try {
-//                entryName = view.getResources().getResourceEntryName(view.getId());
-//            } catch (Resources.NotFoundException ignore) {
-//            }
-//        }
-//        if (!TextUtils.isEmpty(entryName)) {
-//            TrackPoint.onLongClick(className, String.format("R.id.%s", entryName));
-//        }
-//        joinPoint.proceed();
-//    }
+
+    @Pointcut("execution(* android.view.View.OnLongClickListener.onLongClick(..))")
+    public void onViewLongClick() {
+
+    }
+
+    /**
+     * 拦截全局长按事件
+     *
+     * @param joinPoint joinPoint
+     * @throws Throwable Throwable
+     */
+    @Around("onViewLongClick()")
+    public Object processLongClickJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = "";
+        Object target = joinPoint.getTarget();
+        if (target != null) {
+            className = target.getClass().getName();
+        }
+        String entryName = "";
+        Object[] args = joinPoint.getArgs();
+        if (args.length > 0 && args[0] instanceof View) {
+            View view = (View) args[0];
+            try {
+                entryName = view.getResources().getResourceEntryName(view.getId());
+            } catch (Resources.NotFoundException ignore) {
+            }
+        }
+        if (!TextUtils.isEmpty(entryName)) {
+            TrackPoint.onLongClick(className, String.format("R.id.%s", entryName));
+        }
+        return joinPoint.proceed();
+    }
 }
