ec0922c26a47fc56c12cca73c9e196e73b720d7a
wangzhen <lavalike@yeah.net>
2020-08-21 18:05:23 +0800

    动态代理

--- app/src/main/java/com/android/exercise/java/proxy/_dynamic/DynamicProxy.java
+++ app/src/main/java/com/android/exercise/java/proxy/_dynamic/DynamicProxy.java
@@ -1,11 +1,11 @@
 package com.android.exercise.java.proxy._dynamic;
 
-import java.lang.reflect.Proxy;
-
 public class DynamicProxy {
     public static void main(String[] args) {
-        RealSubject realSubject = new RealSubject();
-        Subject subject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(), new Class[]{Subject.class}, new ProxyHandler(realSubject));
+//        Subject instance = new RealSubject();
+//        Subject subject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(), new Class[]{Subject.class}, new ProxyHandler<>(instance));
+
+        Subject subject = ProxyClient.create(Subject.class, RealSubject.class);
         subject.doSomething();
     }
 }
--- app/src/main/java/com/android/exercise/java/proxy/_dynamic/ProxyHandler.java
+++ app/src/main/java/com/android/exercise/java/proxy/_dynamic/ProxyHandler.java
@@ -3,18 +3,18 @@ package com.android.exercise.java.proxy._dynamic;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-public class ProxyHandler implements InvocationHandler {
-    private Object proxyObject;
+public class ProxyHandler<T> implements InvocationHandler {
+    private T mProxy;
 
-    public ProxyHandler(Object proxyObject) {
-        this.proxyObject = proxyObject;
+    public ProxyHandler(T proxy) {
+        this.mProxy = proxy;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("准备工作之前");
-        Object invoke = method.invoke(proxyObject, args);
-        System.out.println("工作完成");
-        return invoke;
+    public T invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("-> 准备工作");
+        Object invoke = method.invoke(mProxy, args);
+        System.out.println("-> 工作完成");
+        return (T)invoke;
     }
 }
