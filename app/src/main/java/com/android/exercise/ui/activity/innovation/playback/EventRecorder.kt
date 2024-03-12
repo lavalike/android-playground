3399ae0cad706f44c4d987ef8529cb4c36e78521
wangzhen <zhen51.wang@tcl.com>
2024-03-12 11:14:03 +0800

    注释补充

--- app/src/main/java/com/android/exercise/ui/activity/innovation/playback/EventRecorder.kt
+++ app/src/main/java/com/android/exercise/ui/activity/innovation/playback/EventRecorder.kt
@@ -14,6 +14,7 @@ import java.util.LinkedList
  * @date : 2024/3/8/008
  */
 object EventRecorder {
+    // 事件队列
     private val touchEvents = LinkedList<EventData>()
 
     private var isRecording = false
@@ -24,19 +25,30 @@ object EventRecorder {
 
     fun hasEvents() = touchEvents.isNotEmpty()
 
+    /**
+     * 开始录制
+     */
     fun startRecord() {
+        // 录制状态判断
         if (isRecording) return
         isRecording = true
+        // 记录开启录制时间
         startTime = System.currentTimeMillis()
+        // 先清空事件列表
         touchEvents.clear()
     }
 
+    // 停止录制
     fun stopRecord() {
         if (isRecording) {
             isRecording = false
         }
     }
 
+    /**
+     * 事件收集
+     * @param ev MotionEvent?
+     */
     @SuppressLint("Recycle")
     fun dispatchTouchEvent(ev: MotionEvent?) {
         if (!isRecording) return
@@ -47,27 +59,45 @@ object EventRecorder {
         touchEvents.add(EventData(event = obtain, delay = delay))
     }
 
+    /**
+     * 事件回放
+     * @param activity Activity
+     */
     fun playback(activity: Activity) {
+        // 回放状态判断，防止重复操作
         if (isRecording) return
         isPlaying = true
+        // 遍历事件队列，回放事件
         touchEvents.forEach { event ->
+            // 按指定时间间隔向Activity分发事件
             handler.postDelayed({
                 activity.dispatchTouchEvent(event.event)
             }, event.delay)
         }
     }
 
+    /**
+     * 停止回放
+     */
     fun stopPlayback() {
         isPlaying = false
+        // 移除所有未执行的事件
         handler.removeCallbacksAndMessages(null)
     }
 
+    /**
+     * 清除事件（MotionEvent回收、缓存清理）
+     */
     fun clear() {
+        // 如果正在录制，则先停止录制
         stopRecord()
+        // 如果正在回放，则先停止回放
         stopPlayback()
         touchEvents.forEach {
+            // 回收伪造的事件
             it.event?.recycle()
         }
+        // 清理缓存
         touchEvents.clear()
     }
 }
